99e825d4d55ee0a6aaed419e247a7a32da20b997
gdetant <gert.detant@gmail.com>
2017-10-22 12:43:08 +0200

    Added GetPersons

--- PersonAPI/Controllers/PersonsController.cs
+++ PersonAPI/Controllers/PersonsController.cs
@@ -1,18 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using PersonAPI.Model;
 
 namespace PersonAPI.Controllers
 {
     [Route("api/Persons")]
     public class PersonsController : Controller
     {
+        [HttpGet()]
         public IActionResult GetPersons()
         {
+            var persons = new List<PersonDto>()
+            {
+                new PersonDto(1, "Gert", "de Tant"),
+                new PersonDto(2, "Ilse", "Bekaert")
+            };
 
-            return Ok();
+            return Ok(persons);
         }
 
     }
